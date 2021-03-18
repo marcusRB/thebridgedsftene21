78852f745c9f8797777dc464654ac1fddfd12758
anderTheBridge <ander@thebridge.tech>
2021-03-18 09:14:40 +0100

    tree and knn

---
+++ 3-Machine Learning/1-Supervisado/KNN/knn.py
@@ -0,0 +1,30 @@
+from scipy.spatial import distance
+import numpy as np
+
+class Knn:
+
+    def __init__(self):
+
+        # Construye tu predictor, debes indicarle definir la k
+
+        pass
+
+
+    def fit(self):
+
+        # verifica que los datos que has introducido tienen sentido
+
+        # Almacena los puntos y las etiquetas con las que haremos la prediccion
+
+        pass
+
+
+    def predict(self):
+
+        # Calcula la distancia del nuevo punto a los demas puntos
+
+        # Evalua los 'votos' de los k vecinos más cercanos
+
+        # Predice
+
+        pass
